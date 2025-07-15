249a23e72cbd040b1d4efa9a1a27a98a8d7a9cf0
Nitesh Vishwakarma <145284923+Nitesh7891@users.noreply.github.com>
2025-07-15 14:58:01 +0530

    Delete web directory

--- web/question1/jv.js
+++
@@ -1,7 +0,0 @@
-var help=document.querySelector("button");
-var par=document.querySelector("p");
-
-help.addEventListener("click",function(){
-    par.textContent="how can i help you sir!";
-    help.style.opacity=0;
-})
--- web/question9/count.js
+++
@@ -1,5 +0,0 @@
-var text=document.querySelector("textarea");
-var count=document.querySelector("span");
-text.addEventListener("input",function(){
-count.textContent=text.value.length;
-})
